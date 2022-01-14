057295f1bf69e7ca8fb56137d31bc682c2a7a574
dogustuluk <dogus.tuluk@gmail.com>
2022-01-15 01:55:20 +0300

    try a category list

---
+++ MvcProje/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProje.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(); //bu yapıyla beraber tüm katmanları kullanmış olduk. >> GetAll() metoduna sağ tık yapıp go to definition 
+                                                    //dersek dataaccesslayer'daki CategoryManager tüm yapıyı çağırmaktadır.
+
+
+        public ActionResult Index()
+        {
+            var categoryvalues = cm.GetAll();
+            return View(categoryvalues);
+        }
+    }
+}
