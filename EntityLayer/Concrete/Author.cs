973ee511bf292af98c828318b6544028e50eca4c
dogustuluk <dogus.tuluk@gmail.com>
2022-01-15 00:13:57 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+
+        [Key]
+        public int AuthorId { get; set; }
+        
+        [StringLength(50)]
+        public string AuthorName { get; set; }
+
+
+        [StringLength(100)]
+        public string AuthorImage { get; set; }
+
+        [StringLength(250)]
+        public string AuthorAbout { get; set; }
+
+
+        ////Yazarlar ile blogları ilişikili hale getirmiş olduk.
+        public ICollection<Blog> Blogs { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [StringLength(30)]
+        public string CategoryName { get; set; }
+
+
+        ////Kategoriler ile blogları ilişikili hale getirmiş olduk.
+        public ICollection<Blog> Blogs { get; set; }
+    }
+}
