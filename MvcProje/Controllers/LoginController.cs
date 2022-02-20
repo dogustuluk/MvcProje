34d4c7da480b6d27976dd1eb347781a74d3507f3
dogustuluk <dogus.tuluk@gmail.com>
2022-02-20 19:14:57 +0300

    author login is created

--- MvcProje/Controllers/LoginController.cs
+++ MvcProje/Controllers/LoginController.cs
@@ -1,17 +1,38 @@
-﻿using System;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcProje.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult AuthorLogin()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AuthorLogin(Author p)
+        {
+            Context context = new Context();
+            var userinfo = context.Authors.FirstOrDefault(x => x.Mail == p.Mail && 
+                                                               x.Password == p.Password);
+            if (userinfo !=null)
+            {
+                FormsAuthentication.SetAuthCookie(userinfo.Mail, false);
+                Session["Mail"] = userinfo.Mail.ToString();
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                return RedirectToAction("AuthorLogin","Login");
+            }
         }
     }
 }
