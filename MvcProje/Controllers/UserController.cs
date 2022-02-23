9720216cb0dd01fd7d731dcdf7f6883960a94925
dogustuluk <dogus.tuluk@gmail.com>
2022-02-24 01:04:59 +0300

    author blog list view on author profile page

--- MvcProje/Controllers/UserController.cs
+++ MvcProje/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,14 @@ namespace MvcProje.Controllers
             mail = (string)Session["Mail"];
             var profilvalues = userProfile.GetAuthorByMail(mail);
             return PartialView(profilvalues);
+        }
+        public ActionResult BlogList(string mail)
+        {
+            mail = (string)Session["Mail"];
+            Context context = new Context();
+            int id = context.Authors.Where(x => x.Mail == mail).Select(y => y.AuthorId).FirstOrDefault();
+            var blogs = userProfile.GetBlogByAuthor(id);
+            return View(blogs);
         }
     }
 }
