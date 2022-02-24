9afbfe6bbd41a48a79ebb4399ad5b112aefc9b02
dogustuluk <dogus.tuluk@gmail.com>
2022-02-25 00:25:52 +0300

    logout

--- MvcProje/Controllers/UserController.cs
+++ MvcProje/Controllers/UserController.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcProje.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         // GET: User
@@ -89,6 +91,12 @@ namespace MvcProje.Controllers
         {
             blogmanager.BlogAddBL(blog);
             return RedirectToAction("BlogList");
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("AuthorLogin", "Login");
         }
     }
 }
