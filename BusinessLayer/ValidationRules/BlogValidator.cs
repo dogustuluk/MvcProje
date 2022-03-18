03f3af5fc483f610e0cfa92046118420aa69c0e5
dogustuluk <dogus.tuluk@gmail.com>
2022-03-18 18:47:26 +0300

    adding a fluentvalidator and validation rules created(blog and category)

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator: AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog başlığı boş bırakılamaz");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog içeriği boş bırakılamaz");
+            RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Blog başlığı en az 5 karakter olmalıdır");
+            RuleFor(x => x.BlogTitle).MaximumLength(100).WithMessage("Blog başlığı en fazla 100 karakter olmalıdır");
+        }
+    }
+}
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator: AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori alanı boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Kategori açıklaması boş geçilemez");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen en az 3 karakterlik bir kategori ismi girin");
+            RuleFor(x => x.CategoryName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik bir kategori ismi girin");
+        }
+    }
+}
